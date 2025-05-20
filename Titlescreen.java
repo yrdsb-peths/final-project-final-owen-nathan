fb51eed800948ca827f2e027f4874b51924bfe49
nthnj <349045377@gapps.yrdsb.ca>
2025-05-20 14:49:09 -0400

    title and start instructions

--- Titlescreen.java
+++ Titlescreen.java
@@ -8,7 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Titlescreen extends World
 {
-
+    Label titleLabel = new Label("Planet of Men", 80);
+    Label start = new Label("Press space to start", 30);
     /**
      * Constructor for objects of class Titlescreen.
      * 
@@ -18,5 +19,14 @@ public class Titlescreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);  
         setBackground("images/titleScreen.jpg");
+        addObject(titleLabel, 300, 200);
+        addObject(start, 300, 250);
+    }
+    
+    public void act() {
+        if(Greenfoot.isKeyDown("space")) {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
     }
 }
