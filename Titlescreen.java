9b3ffc785c75a726686a45d61ede7c0628edeb64
owenchlee <owenlees888@gmail.com>
2025-05-20 14:38:08 -0400

    Add title

--- Titlescreen.java
+++ Titlescreen.java
@@ -18,5 +18,15 @@ public class Titlescreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);  
         setBackground("images/titleScreen.jpg");
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Label label = new Label("100 Men Vs. 1 Gorilla", 50);
+        addObject(label,289,192);
     }
 }
