1a9a128dafbed75c7158363328c19f87284c46f6
nthnj <349045377@gapps.yrdsb.ca>
2025-05-23 09:57:38 -0400

    asteroid into myworld

--- Asteroid.java
+++ Asteroid.java
@@ -1,34 +1,26 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
-/**
- * Write a description of class Asteroid here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Asteroid extends Actor
 {
-    /**
-     * Act - do whatever the Asteroid wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public static int x = 0;
+
     public void act()
     {
-        // Add your action code here.
         setLocation(getX(), getY() + 3);
-        
+
         if(Greenfoot.isKeyDown("left")) {
             move(-2);
         } else if(Greenfoot.isKeyDown("right")) {
             move(2);
         }
-        
-        Cutscene1 cutscene = (Cutscene1) getWorld();
-        if(getY() >= 399) {
-            cutscene.removeObject(this);
+
+        if (getWorld() instanceof Cutscene1 && getY() >= 399) {
+            x = getX();
+            getWorld().removeObject(this);
+            Greenfoot.setWorld(new MyWorld());
         }
     }
-    
+
     public Asteroid() {
         GreenfootImage asteroid = new GreenfootImage("images/asteroid.png");
         asteroid.scale(40, 40);
--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,13 @@
-    import greenfoot.*;
+import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        GreenfootImage worldBG = new GreenfootImage("images/dgggoyk-fdd28b15-79e9-4a3d-a8bd-d7d966e77900.jpg");
+        worldBG.scale(600, 400);
+        setBackground(worldBG);
+        
+        Asteroid asteroid = new Asteroid();
+        addObject(asteroid, Asteroid.x, 0);
     }
 }
