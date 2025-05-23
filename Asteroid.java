7e2316ea01c241619e64afe00beebcf5d7eca027
nthnj <349045377@gapps.yrdsb.ca>
2025-05-23 14:11:21 -0400

    explosion and gorilla spawn

--- Asteroid.java
+++ Asteroid.java
@@ -1,23 +1,38 @@
 import greenfoot.*;
 
-public class Asteroid extends Actor
-{
+public class Asteroid extends Actor {
     public static int x = 0;
-
-    public void act()
-    {
+    
+    public void act() {
         setLocation(getX(), getY() + 3);
 
-        if(Greenfoot.isKeyDown("left")) {
+        if (Greenfoot.isKeyDown("left")) {
             move(-2);
-        } else if(Greenfoot.isKeyDown("right")) {
+        } else if (Greenfoot.isKeyDown("right")) {
             move(2);
         }
 
+        // If we're in Cutscene1 and asteroid reaches bottom, transition to MyWorld
         if (getWorld() instanceof Cutscene1 && getY() >= 399) {
-            x = getX();
+            x = getX(); // Save the x-position
             getWorld().removeObject(this);
             Greenfoot.setWorld(new MyWorld());
+            return;
+        }
+
+        // Spawn gorilla and remove asteroid at Y >= 200
+        if (getWorld() instanceof MyWorld && getY() >= 200) {
+            MyWorld world = (MyWorld) getWorld();
+            world.landed = true;
+
+            int asteroidX = getX(); // save X before removing the asteroid
+
+            Gorilla gorilla = new Gorilla();
+            Boom boom = new Boom(asteroidX, 200);
+            world.addObject(boom, asteroidX, 200);
+
+            world.removeObject(this); // now safe to remove
+            return;
         }
     }
 
--- MyWorld.java
+++ MyWorld.java
@@ -1,20 +1,18 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    public boolean landed = false;
+
     public MyWorld() {
         super(600, 400, 1);
 
-        // Set background image
+        // Set background
         GreenfootImage worldBG = new GreenfootImage("images/dgggoyk-fdd28b15-79e9-4a3d-a8bd-d7d966e77900.jpg");
         worldBG.scale(600, 400);
         setBackground(worldBG);
-        
-        // Add asteroid at saved x position
+
+        // Add asteroid at saved x-position
         Asteroid asteroid = new Asteroid();
         addObject(asteroid, Asteroid.x, 0);
-
-        // Add gorilla
-        Gorilla gorilla = new Gorilla();
-        addObject(gorilla, 300, 300);
     }
 }
